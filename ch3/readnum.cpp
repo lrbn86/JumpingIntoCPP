e9ba9d4d7ce555de0f6802673f3ef168d22e1bd2
brandon <lrbn86@gmail.com>
2017-06-22 00:06:46 -0400

    Studying ch3

---
+++ ch3/readnum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int this_is_a_number;
+    cout << "Please enter a number: ";
+    cin >> this_is_a_number;
+    cout << "You entered: " << this_is_a_number << endl;
+}
